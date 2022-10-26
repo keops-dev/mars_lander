5256a8f27c0b9911675144b81a820b2e251c4c49
keops-dev <pierjcb@gmail.com>
2022-10-26 09:51:01 +0200

    remove the loading of entrees.txt

--- src/readline.ts
+++ src/readline.ts
@@ -1,12 +1,22 @@
-import FICHIER_ENTREES from "bundle-text:./entrees.txt"
+const ENTREES = [
+  "7",
+  "0 100",
+  "1000 500",
+  "1500 1500",
+  "3000 1000",
+  "4000 150",
+  "5500 150",
+  "6999 800",
+  "2500 2700 0 0 550 0 0",
+]
 
 // On crée le générateur pour lire le fichier
-let reader = fileReader(FICHIER_ENTREES)
+let reader = fileReader(ENTREES)
 
 // Générateur pour lire une par une les lignes du fichier des entrées
 function* fileReader(fichier) {
-  const lignes = fichier.split("\n")
-  for (ligne of lignes) {
+  const lignes = fichier
+  for (const ligne of lignes) {
     yield ligne
   }
 }
